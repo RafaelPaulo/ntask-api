fb43d1bc19ab49b1516c87e369a912cca0eb1ba6
RafaelPaulo <rafael-sq@hotmail.com>
2017-01-18 23:52:34 -0200

    GZIP and HTTTPs certification

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser'
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import compression from 'compression'
 import logger from './logger.js'
 
 module.exports = app => {
@@ -19,6 +20,7 @@ module.exports = app => {
         method: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeader: ['Content-type', 'Authorization']
     }))
+    app.use(compression())
     app.use(bodyParser.json())
     app.use(app.auth.initialize())
     app.use((req, res, next) => {
